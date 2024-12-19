3fe1c8c6a3dcf9470c7cb09157c1ff4068df5098
matydiop <maty0012.dp@gmail.com>
2024-12-19 10:31:09 +0100

    Initial commit

---
+++ db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize')
+const dbUrl = process.env.DATABASE_URL;
+// Database
+const sequelize = new Sequelize(
+  {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      createdAt: 'added',
+      updatedAt: 'updated',
+    }
+  },
+)
+
+sequelize.authenticate()
+sequelize.sync()
+
+module.exports = sequelize
